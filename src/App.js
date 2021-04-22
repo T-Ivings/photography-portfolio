e0e0f32116ff5560ecba42d91d5bccf50c6c4ce1
Taylor <ivingstaylor@gmail.com>
2021-04-21 21:03:37 -0700

    code formatting

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <Navigation />
       <header className="App-header">
         <Slides />
-
       </header>
     </div>
   );
--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -7,19 +7,22 @@ const Navigation = () => {
 return (
 <Fade top duration={3500}>
   <Navbar fixed="top" className="navbar">
+
     <Nav justify className="navbar-links">
       <Nav.Link>About</Nav.Link>
       <Nav.Link>FAQ</Nav.Link>
       <Nav.Link>Portfolio</Nav.Link>
-      </Nav>
-      <Navbar.Brand><img src={logo} width='300px'/></Navbar.Brand>
-      <Nav justify className="navbar-links">
+    </Nav>
+
+    <Navbar.Brand><img src={logo} width='300px'/></Navbar.Brand>
+      
+    <Nav justify className="navbar-links">
       <Nav.Link>Kind Words</Nav.Link>
       <Nav.Link>Contact</Nav.Link>
       <Nav.Link>Blog</Nav.Link>
     </Nav>
   </Navbar>
-  </Fade>
+</Fade>
 )
 }
 
