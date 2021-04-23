6c49391fb39553fd7286ccc502cc53e07b6bd47c
Taylor <ivingstaylor@gmail.com>
2021-04-22 22:01:15 -0700

    changes carousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.scss';
-import Hero from './component/hero/Hero'
-import Navigation from './component/navigation/Navigation'
-import Slides from './component/slideshow/Slides'
+import Hero from './component/hero/Hero';
+import Navigation from './component/navigation/Navigation';
+import Slides from './component/slideshow/Slides';
+import { Container } from 'react-bootstrap';
 
 function App() {
+
   return (
     <div className="App">
       <Navigation />
       <header className="App-header">
-        <Slides />
       </header>
+      <Container>
+        <Slides />
+      </Container>
     </div>
+  
   );
 }
 
