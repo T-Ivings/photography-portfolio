4ba0739a2d1c67ae45ffb313975ccb86fd9ec1be
Taylor <ivingstaylor@gmail.com>
2021-04-28 16:20:45 -0700

    divider component

---
+++ src/component/divider/Divide.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Divider } from '@material-ui/core';
+
+const Divide = () => {
+  return (
+    <Container><br/><br/><br/><br/><br/>
+      <Divider variant="middle" /><br/><br/><br/><br/><br/>
+      <div>Some text telling potential clients what you do</div><br/>
+      <div>What you specialize in</div><br/>
+      <div>IDK write something here to we have a blank space</div><br/><br/><br/><br/><br/><br/>
+      <Divider variant="middle" /><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </Container>
+  )
+}
+export default Divide;
