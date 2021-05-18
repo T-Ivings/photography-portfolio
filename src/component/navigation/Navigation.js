aac5a8439ffb54387a4b295df9499bdbf0f205d9
Taylor <ivingstaylor@gmail.com>
2021-05-17 23:46:07 -0700

    started mobile development

--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -1,13 +1,16 @@
 import { React, useState, useEffect} from 'react';
 import { Navbar, Nav} from "react-bootstrap";
 import { Fade } from 'react-reveal';
-import logo from "../../images/carlena-ivings_web.jpg"
+import logo from "../../images/carlena-ivings_web.jpg";
 import { debounce } from '../../utilities/debounce'; 
+import './Navigation.scss';
 
 const Navigation = () => {
 
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
 
 
   const handleScroll = debounce(() =>  {
@@ -18,13 +21,37 @@ const Navigation = () => {
     setPrevScrollPos(currentScrollPos);
   });
 
+  const styles = {
+    'desktop': {
+
+    },
+    'mobile': {
+    
+    }
+  };
+
+  const responsiveSize = () => {
+    return isMobile ? 250 : 450;
+  }
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
     return () => window.removeEventListener('scroll', handleScroll);
   }, [prevScrollPos, visible, handleScroll])
 
+useEffect(() => {
+  if (window.innerWidth > 769) {
+    setIsDesktop(true);
+    setIsMobile(false);
+  } else {
+    setIsDesktop(false);
+    setIsMobile(true);
+  }
 
+},[]);
+
+console.log(responsiveSize());
   return (
     <Navbar style={{top: visible ? '0' : '-160px', transition: 'top 0.6s'}} fixed={"top"} className="navbar">
       <Nav className='d-flex justify-content-around' id="navbar-links">
@@ -32,7 +59,7 @@ const Navigation = () => {
           <Nav.Link>About</Nav.Link> 
           <Nav.Link>FAQ</Nav.Link>
           <Nav.Link>Portfolio</Nav.Link>
-          <Navbar.Brand><img src={logo} width='300px'/></Navbar.Brand>
+          <Navbar.Brand><img src={logo} width={(() => {return isMobile ? 250 : 450})} style={{responsiveSize}}/></Navbar.Brand>
           <Nav.Link>Packages</Nav.Link> 
           <Nav.Link>Testimony</Nav.Link> 
           <Nav.Link>Blog</Nav.Link>
