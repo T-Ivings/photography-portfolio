a6eb2549c6570821a8094e826904e7343f0b0fd1
Taylor <ivingstaylor@gmail.com>
2021-05-26 09:22:01 -0700

    pushed for big comp

--- src/component/faq/Faq.js
+++ src/component/faq/Faq.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { Accordion , Card} from 'react-bootstrap';
+import { Accordion, Card} from 'react-bootstrap';
 import './Faq.scss';
 
 const Faq = () => {
+
+  const frequentQuestions = ["Question One", "Question Two", "Question Three", "Question Four", "Question Five", "Question Six", "Question Seven", "Question Eight", "Question Nine", "Question Ten",];
+
+
+  const mappedQuestions = frequentQuestions.map((question, index) => {
+    
+      return (
+        <Accordion.Toggle as={Card.Header} eventKey={index}>
+          {question}
+        </Accordion.Toggle>
+      );
+  });
+
+  const mappedAnswers = frequentQuestions.map((answer, index))
 
   return (
     <section id="faq">
       <h1>Frequently Asked Questions</h1>
 
-      <Accordion class>
-        <Card>  
-          <Accordion.Toggle as={Card.Header} eventKey={0}>
-            First Questions
-          </Accordion.Toggle>
-          
+      <Accordion className="accordion">
+        <Card>
+          {mappedQuestions}
+          {}
         </Card>
       </Accordion>
     </section>
